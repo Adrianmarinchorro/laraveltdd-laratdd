93c70d9b56ea0446f62a83fd8c812eacfd0b68d2
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2022-11-28 13:20:46 +0100

    step 3: getting started with laravel routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inicio', function () {
+    return 'Hola Mundo';
+});
+
+Route::get('/usuarios', function () {
+   return 'Mostrando listado de los usuarios';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return 'Mostrando detalle del usuario ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('usuarios/nuevo', function () {
+   return 'creando nuevo usuario';
+});
+
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null){
+
+    if($nickname) {
+        return 'saludos ' . $name . ', tu apodo es ' . $nickname;
+    } else {
+        return 'saludos ' . $name . ', no tienes apodo.';
+    }
+
+
+
+});
