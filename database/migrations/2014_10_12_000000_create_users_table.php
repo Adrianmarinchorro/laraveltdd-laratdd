3b5b70fe197ba11e7655f339d0f008e92359abd5
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2022-12-06 20:28:23 +0100

    step 9: Introduction database with migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->increments('id'); // Integer Unsigned Autoincrement
+            $table->string('name'); // Varchar
+            $table->string('email')->unique(); // Varchar y Unique
+            $table->string('password'); // Varchar
+            $table->rememberToken(); // metodo helper para columna comun en la app como ej almacenar token para recordar usuarios.
+            $table->timestamps(); // helper que genera las columnas updated_at y created_at
         });
     }
 
