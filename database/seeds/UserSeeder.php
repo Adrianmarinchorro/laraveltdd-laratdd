e4af52f7fa86d0900fd74b4996de780d9e1d9e64
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2022-12-08 14:12:20 +0100

    step 13: SQL query constructor

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,10 +12,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        //$professions = DB::select('SELECT id FROM professions WHERE title = ? LIMIT 0,1', ['Desarrollador back-end']);
+
+//        $profession = DB::table('professions')
+//            ->select('id')
+//            ->where('title', '=', 'Desarrollador back-end')
+//            ->first()
+//            // ->take(1)->get()
+//        ;
+
+        $professionId = DB::table('professions')
+            ->whereTitle('Desarrollador back-end') // metodo magico
+            // ->where('title', 'Desarrollador back-end')
+            ->value('id')
+        ;
+
+        // dd($profession->id); // $professions[0]
+
         DB::table('users')->insert([
             'name' => 'adrian',
             'email' => 'adri@gmail.com',
             'password' => bcrypt('123'),
+            'profession_id' => $professionId, // $profession->id
         ]);
     }
 }
