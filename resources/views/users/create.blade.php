a69635cd07ce82cfcee925ee71f3900a07b40706
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2022-12-27 13:11:44 +0100

    step 40: added style at form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,49 +3,49 @@
 @section('title', 'Crear usuario');
 
 @section('content')
-    <h1>Crear usuario:</h1>
-
-    @if($errors->any())
-        <div class="alert alert-danger">
-            <h5>Por favor corrige los errores de debajo:</h5>
-            <ul>
-            @foreach($errors->all() as $error)
-
-            <li>{{$error}}</li>
-
-            @endforeach
-            </ul>
+    <div class="card">
+        <div class="card-header h4">
+            Crear nuevo usuario
         </div>
-    @endif
 
-    <form action="{{ route('users.store') }}" method="POST">
-        {{ csrf_field() }}
-        {{-- {!! csrf_field() !!}--}}
+        <div class="card-body">
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <h5>Por favor corrige los errores de debajo:</h5>
+                    <ul>
+                        @foreach($errors->all() as $error)
 
-        <label for="name">Nombre:</label>
-        <input type="text" name="name" id="name" placeholder="Pepe Pérez" value="{{ old('name') }}">
+                            <li>{{$error}}</li>
 
-{{--        @if($errors->has('name'))--}}
-{{--            <p>{{ $errors->first('name') }}</p>--}}
-{{--        @endif--}}
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-        <br>
+            <form action="{{ route('users.store') }}" method="POST">
+                {{ csrf_field() }}
+                {{-- {!! csrf_field() !!}--}}
 
-        <label for="email">Correo electrónico:</label>
-        <input type="email" name="email" id="email" placeholder="pedro@example.com" value="{{ old('email') }}">
+                <div class="form-group">
+                    <label for="name">Nombre:</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Pepe Pérez" value="{{ old('name') }}">
+                </div>
 
-        <br>
+                <div class="form-group">
+                    <label for="email">Correo electrónico:</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="pedro@example.com" value="{{ old('email') }}">
 
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" placeholder="Mayor a 6 caracteres">
+                </div>
 
-        <br>
+                <div class="form-group">
+                    <label for="password">Contraseña:</label>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Mayor a 6 caracteres">
 
-        <button type="submit">Crear usuario</button>
-    </form>
+                </div>
 
-    <p>
-        <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
-    </p>
-
+                <button class="btn btn-primary" type="submit">Crear usuario</button>
+                <a class="btn btn-link" href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
+            </form>
+        </div>
+    </div>
 @endsection
