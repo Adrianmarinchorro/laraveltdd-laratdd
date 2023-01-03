cf9632d698d82b2dbd18988363c8ef08f1c3444a
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-01-04 00:08:58 +0100

    step 13: share data between views of Laravel with View Composer

---
+++ app/Http/ViewComposers/UserFieldsComposer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use App\Profession;
+use App\Skill;
+use Illuminate\Contracts\View\View;
+
+class UserFieldsComposer
+{
+
+    public function compose(View $view)
+    {
+        $professions = Profession::orderBy('title','ASC')->get();
+
+        $skills = Skill::orderBy('name', 'ASC')->get();
+
+        $roles = trans('users.roles');
+
+        return $view->with(compact('professions', 'skills', 'roles'));
+
+    }
+
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use App\Http\ViewComposers\UserFieldsComposer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +18,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::component('shared._card', 'card');
+
+        View::composer(['users.create', 'users.edit'], UserFieldsComposer::class);
+        //View::composer('users/_fields', UserFieldsComposer::class); // esto para pasarlo solo a los campos, pero daría error en los test
     }
 
     /**
