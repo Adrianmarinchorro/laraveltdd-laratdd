fc10f28ee9ab675bc56968d172191f2c096f900b
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-01-28 18:49:32 +0100

    step 43. Generar URLs con orden dinámico en Laravel

--- app/Sortable.php
+++ app/Sortable.php
@@ -2,11 +2,19 @@
 
 namespace App;
 
+use Illuminate\Support\Arr;
+
 class Sortable
 {
 
     protected $currentColumn;
     protected $currentDirection;
+    protected $currentUrl;
+
+    public function __construct($currentUrl)
+    {
+        $this->currentUrl = $currentUrl;
+    }
 
     public function setCurrentOrder($column, $direction = 'asc')
     {
@@ -14,13 +22,33 @@ class Sortable
         $this->currentDirection = $direction;
     }
 
+    public function url($column)
+    {
+        if ($this->isSortingBy($column, 'asc')) {
+            return $this->buildSortableUrl($column, 'desc');
+        }
+
+        return $this->buildSortableUrl($column, 'asc');
+
+    }
+
+    protected function buildSortableUrl($column, $direction = 'asc')
+    {
+        return $this->currentUrl . '?' . Arr::query(['order' => $column, 'direction' => $direction]);
+    }
+
+    protected function isSortingBy($column, $direction)
+    {
+        return $this->currentColumn == $column && $this->currentDirection == $direction;
+    }
+
     public function classes($column)
     {
-        if($this->currentColumn == $column && $this->currentDirection == 'asc') {
+        if ($this->isSortingBy($column, 'asc')) {
             return 'link-sortable link-sorted-up';
         }
 
-        if($this->currentColumn == $column && $this->currentDirection == 'desc') {
+        if ($this->isSortingBy($column, 'desc')) {
             return 'link-sortable link-sorted-down';
         }
 
--- tests/Unit/SortableTest.php
+++ tests/Unit/SortableTest.php
@@ -9,36 +9,54 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SortableTest extends TestCase
 {
-    use RefreshDatabase;
+    protected $sortable;
+
+    protected function setUp(): void
+    {
+        parent::setUp(); // ejecuta lo que hay en el padre
+
+        $this->sortable = new Sortable('http://laratdd.local/demo');
+    }
 
     /** @test */
     function return_a_css_class_to_indicate_the_column_is_sortable()
     {
-        $sortable = new Sortable();
-
         // decimos que la clase de css es igual que lo que obtiene del objeto sortable cuando llama al metodo classes al recibir el parametro
-        $this->assertSame('link-sortable', $sortable->classes('first_name'));
+        $this->assertSame('link-sortable', $this->sortable->classes('first_name'));
     }
 
 
     /** @test */
     function return_a_css_class_to_indicate_the_column_is_sorted_in_ascendent_order()
     {
-        $sortable = new Sortable();
+        $this->sortable->setCurrentOrder('first_name'); //establece el orden por esa columna
 
-        $sortable->setCurrentOrder('first_name'); //establece el orden por esa columna
-
-        $this->assertSame('link-sortable link-sorted-up', $sortable->classes('first_name'));
+        $this->assertSame('link-sortable link-sorted-up', $this->sortable->classes('first_name'));
     }
 
     /** @test */
     function return_a_css_class_to_indicate_the_column_is_sorted_in_descendent_order()
     {
-        $sortable = new Sortable();
+        $this->sortable->setCurrentOrder('first_name', 'desc'); //establece el orden de esa columna en este momento
 
-        $sortable->setCurrentOrder('first_name', 'desc'); //establece el orden de esa columna en este momento
+        $this->assertSame('link-sortable link-sorted-down', $this->sortable->classes('first_name'));
+    }
 
-        $this->assertSame('link-sortable link-sorted-down', $sortable->classes('first_name'));
+    /** @test */
+    function build_a_url_with_sortable_data()
+    {
+        //generamos el objeto con el constructor recibiendo una url
+        //$sortable = new Sortable('http://laratdd.local/demo');
+
+        $this->assertSame('http://laratdd.local/demo?order=first_name&direction=asc', $this->sortable->url('first_name'));
+    }
+
+    /** @test */
+    function build_a_url_with_desc_order_if_the_current_column_matches_the_given_one_and_the_current_direction_is_asc()
+    {
+        $this->sortable->setCurrentOrder('first_name', 'asc');
+
+        $this->assertSame('http://laratdd.local/demo?order=first_name&direction=desc', $this->sortable->url('first_name'));
     }
 
 }
